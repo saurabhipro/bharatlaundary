30eb6bbb625613243499385317993d6b5b8d97fd
saurabhipro <saurabh.mehrotrag@gmail.com>
2026-02-21 14:01:10 +0530

    refactor: Change `otp_message_template` field type from Text to Char.

--- bharatlaundary/models/res_config_settings.py
+++ bharatlaundary/models/res_config_settings.py
@@ -8,7 +8,7 @@ class ResConfigSettings(models.TransientModel):
     otp_client_id = fields.Char(string='OTP Client ID', config_parameter='bharatlaundary.otp_client_id')
     otp_sender_id = fields.Char(string='OTP Sender ID', config_parameter='bharatlaundary.otp_sender_id')
     dlt_template_id = fields.Char(string='DLT Template ID', config_parameter='bharatlaundary.dlt_template_id')
-    otp_message_template = fields.Text(string='OTP Message Template', config_parameter='bharatlaundary.otp_message_template')
+    otp_message_template = fields.Char(string='OTP Message Template', config_parameter='bharatlaundary.otp_message_template')
     android_app_hash = fields.Char(string='Android App Hash', config_parameter='bharatlaundary.android_app_hash')
     test_mobile_number = fields.Char(string='Test Mobile Number', config_parameter='bharatlaundary.test_mobile_number')
 
