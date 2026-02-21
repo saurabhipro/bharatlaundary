e167ad3af11ba779df6344494f1724213f5d9a3a
saurabhipro <saurabh.mehrotrag@gmail.com>
2026-02-21 14:12:05 +0530

    refactor: Update OTP API configuration settings, including `base_setup` dependency, refined field labels, and view layout adjustments.

--- bharatlaundary/__manifest__.py
+++ bharatlaundary/__manifest__.py
@@ -10,7 +10,7 @@ Handles backend logic for Bharat Laundary.
     'author': "Bharat Laundary Team",
     'website': "http://www.bharatlaundary.com",
     'category': 'Services',
-    'depends': ['base', 'product', 'website', 'website_sale', 'payment_custom', 'hr', 'sale', 'account', 'barcodes', 'crm', 'sale_crm'],
+    'depends': ['base', 'base_setup', 'product', 'website', 'website_sale', 'payment_custom', 'hr', 'sale', 'account', 'barcodes', 'crm', 'sale_crm'],
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
--- bharatlaundary/models/res_config_settings.py
+++ bharatlaundary/models/res_config_settings.py
@@ -3,12 +3,12 @@ from odoo import models, fields
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    otp_api_url = fields.Char(string='OTP API URL', config_parameter='bharatlaundary.otp_api_url')
-    otp_api_key = fields.Char(string='OTP API Key', config_parameter='bharatlaundary.otp_api_key')
-    otp_client_id = fields.Char(string='OTP Client ID', config_parameter='bharatlaundary.otp_client_id')
-    otp_sender_id = fields.Char(string='OTP Sender ID', config_parameter='bharatlaundary.otp_sender_id')
+    otp_api_url = fields.Char(string='API URL', config_parameter='bharatlaundary.otp_api_url')
+    otp_api_key = fields.Char(string='API Key', config_parameter='bharatlaundary.otp_api_key')
+    otp_client_id = fields.Char(string='Client ID', config_parameter='bharatlaundary.otp_client_id')
+    otp_sender_id = fields.Char(string='Sender ID', config_parameter='bharatlaundary.otp_sender_id')
     dlt_template_id = fields.Char(string='DLT Template ID', config_parameter='bharatlaundary.dlt_template_id')
-    otp_message_template = fields.Char(string='OTP Message Template', config_parameter='bharatlaundary.otp_message_template')
+    otp_message_template = fields.Char(string='Message Template', config_parameter='bharatlaundary.otp_message_template')
     android_app_hash = fields.Char(string='Android App Hash', config_parameter='bharatlaundary.android_app_hash')
     test_mobile_number = fields.Char(string='Test Mobile Number', config_parameter='bharatlaundary.test_mobile_number')
 
